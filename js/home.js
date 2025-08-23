e8610d9f8b0c57006d13ac6f5382433447b0da20
S.M ANZUMUL JUBAYER <anzumuljubayer222@gmail.com>
2025-08-23 21:38:32 +0600

    transfer-and-login

--- js/home.js
+++ js/home.js
@@ -35,6 +35,7 @@ document.getElementById('cash-out-btn').addEventListener('click',function(event)
      const agNum=document.getElementById('agent-num').value
      if(agNum.length<11){
         alert('Invalid Number')
+        return
      }
       
      if(pin!==pinNum){
@@ -46,12 +47,55 @@ document.getElementById('cash-out-btn').addEventListener('click',function(event)
      document.getElementById('balance').innerText=mainBalance
   
 })
+// transfer money
+     document.getElementById('transfer-btn').addEventListener('click',function(event){
+        event.preventDefault()
+       const transferNum=document.getElementById('transfer-num').value
+       const transferAmount=parseInt(document.getElementById('transfer-amount').value)
+       const transferpin=parseInt(document.getElementById('transfer-pin').value)
+       const balance=parseInt(document.getElementById('balance').innerText)
+     
+    if(transferNum.length<11){
+        alert('Invalid Number')
+        return
+     } 
+     if(pin!==transferpin){
+        alert('Invalid Pin')
+        return
+    }
+    const mainBalance=balance-transferAmount
+     document.getElementById('balance').innerText=mainBalance
+        
+})
+
+
+
+
+
+
+
+
+
+
 // toggling feature
 document.getElementById('add-click').addEventListener('click',function(){
     document.getElementById('cash-out-parent').style.display="none"
     document.getElementById('add-money-parent').style.display="block"
+    document.getElementById('transfer-parent').style.display="none"
 })
 document.getElementById('cash-out-click').addEventListener('click',function(){
        document.getElementById('add-money-parent').style.display="none"
        document.getElementById('cash-out-parent').style.display="block"
+       document.getElementById('transfer-parent').style.display="none"
+})
+document.getElementById('transfer-click').addEventListener('click',function(){
+       document.getElementById('transfer-parent').style.display="block"
+       document.getElementById('add-money-parent').style.display="none"
+       document.getElementById('cash-out-parent').style.display="none"
+})
+
+
+// log-out
+document.getElementById('log-out').addEventListener('click',function(){
+    window.location.href="./index.html"
 })
