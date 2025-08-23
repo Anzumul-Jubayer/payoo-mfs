00db1c1781245b8f7cd102cb66c2043740b6625b
S.M ANZUMUL JUBAYER <anzumuljubayer222@gmail.com>
2025-08-23 19:28:56 +0600

    add-cashout-added

---
+++ js/home.js
@@ -0,0 +1,57 @@
+const pin=1256
+const accNum=12345678910
+
+// add money btn
+document.getElementById('add-money-btn').addEventListener('click',function(event){
+    event.preventDefault()
+
+    const bank=document.getElementById('bank').value
+    const accountNum=parseInt(document.getElementById('account-num').value)
+    const addMoney=parseInt(document.getElementById('add-money').value)
+    const pinNum=parseInt(document.getElementById('pin-num').value)
+    const balance=parseInt(document.getElementById('balance').innerText)
+    
+    if(bank.includes('Select Bank')){
+        alert('Please Select Bank ')
+        return
+    }
+     if(accNum!==accountNum){
+        alert('Please provide a valid bank number')
+        return
+    }else if(pin!==pinNum){
+        alert('Invalid Pin')
+        return
+    }
+    const mainBalance=addMoney+balance
+    document.getElementById('balance').innerText=mainBalance
+  
+})
+// cash-out-btn
+document.getElementById('cash-out-btn').addEventListener('click',function(event){
+     event.preventDefault()
+     const balance=parseInt(document.getElementById('balance').innerText)
+     const withdrawAmount=parseInt(document.getElementById('cash-out').value)
+     const pinNum=parseInt(document.getElementById('out-pin-num').value)
+     const agNum=document.getElementById('agent-num').value
+     if(agNum.length<11){
+        alert('Invalid Number')
+     }
+      
+     if(pin!==pinNum){
+        alert('Invalid Pin')
+        return
+    }
+
+     const mainBalance=balance-withdrawAmount
+     document.getElementById('balance').innerText=mainBalance
+  
+})
+// toggling feature
+document.getElementById('add-click').addEventListener('click',function(){
+    document.getElementById('cash-out-parent').style.display="none"
+    document.getElementById('add-money-parent').style.display="block"
+})
+document.getElementById('cash-out-click').addEventListener('click',function(){
+       document.getElementById('add-money-parent').style.display="none"
+       document.getElementById('cash-out-parent').style.display="block"
+})
